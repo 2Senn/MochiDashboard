7e2ebf4162ea3a20621189015ab3c9654dfe11df
Faisal <1senn.kj@gmail.com>
2022-08-22 20:07:11 +0300

    feat(setup): setup complete
    finished file structuring as well as component creation for later use

---
+++ src/pages/index.jsx
@@ -0,0 +1,16 @@
+export { default as Ecommerce } from './ecommerce';
+export { default as Kanban } from './kanban';
+export { default as Orders } from './orders';
+export { default as Employees } from './employees';
+export { default as Editor } from './editor';
+export { default as Customers } from './customers';
+export { default as ColorPicker } from './color-picker';
+export { default as Calendar } from './calendar';
+export { default as Area } from './Charts/area';
+export { default as Bar } from './Charts/bar';
+export { default as ColorMapping } from './Charts/color-mapping';
+export { default as Financial } from './Charts/financial';
+export { default as LineChart } from './Charts/line-chart';
+export { default as Pie } from './Charts/pie';
+export { default as Pyramid } from './Charts/pyramid';
+export { default as Stacked } from './Charts/stacked';
